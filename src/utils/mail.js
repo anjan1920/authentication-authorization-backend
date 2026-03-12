f4f353f43834bf18dc9af57a6c20197faa297b2e
Anjan Das <anjan1920@gmail.com>
2026-03-12 22:43:56 +0530

    fix

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -1,7 +1,7 @@
 import Mailgen from "mailgen";
 import nodemailer from "nodemailer";
 
-// create transporter once
+// create transporter once (better for Render)
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
   },
   connectionTimeout: 10000,
   greetingTimeout: 10000,
-  socketTimeout: 20000
+  socketTimeout: 20000,
 });
 
 const sendEmail = async (options) => {
@@ -27,21 +27,70 @@ const sendEmail = async (options) => {
   const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
   const emailHtml = mailGenerator.generate(options.mailgenContent);
 
+  const mail = {
+    from: '"Authentication System" <anjandas8427@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+
   try {
-
-    const mail = {
-      from: '"Authentication System" <anjandas8427@gmail.com>',
-      to: options.email,
-      subject: options.subject,
-      text: emailTextual,
-      html: emailHtml,
-    };
-
     const info = await transporter.sendMail(mail);
 
-    console.log("Email sent:", info.messageId);
+    console.log("Email sent successfully:", info.messageId);
 
+    return info;
   } catch (error) {
     console.error("Email sending failed:", error);
+    throw error;
   }
+};
+
+// email verification template
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! We're excited to have you on board.",
+      action: {
+        instructions:
+          "To verify your email please click on the following button:",
+        button: {
+          color: "#22BC66",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+// forgot password template
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We received a request to reset your account password.",
+      action: {
+        instructions:
+          "Click the button below to reset your password:",
+        button: {
+          color: "#22BC66",
+          text: "Reset Password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "If you didn't request a password reset, you can safely ignore this email.",
+    },
+  };
+};
+
+export {
+  sendEmail,
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
 };
