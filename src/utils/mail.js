a2868f949527ba01ac979cecc72f90f72b73e8ff
Anjan Das <anjan1920@gmail.com>
2026-03-12 20:40:35 +0530

    first commit

---
+++ src/utils/mail.js
@@ -0,0 +1,88 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Authentication & Authorization",
+      link: "****",
+    },
+  });
+
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS,
+      },
+    });
+
+    const mail = {
+      from: '"Authentication System" <anjandas8427@gmail.com>',
+      to: options.email,
+      subject: options.subject,
+      text: emailTextual,
+      html: emailHtml,
+    };
+
+    const info = await transporter.sendMail(mail);
+
+    console.log("Email sent:", info.messageId);
+
+  } catch (error) {
+    console.error("Email sending failed:", error);
+  }
+};
+
+const emailVerificationMailgenContent = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! we're excited to have you on board.",
+      action: {
+        instructions:
+          "To verify your email please click on the following button",
+        button: {
+          color: "#22BC66",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+const forgotPasswordMailgenContent = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of your account",
+      action: {
+        instructions:
+          "To reset your password click on the following button or link",
+        button: {
+          color: "#22BC66",
+          text: "Reset password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+export {
+  emailVerificationMailgenContent,
+  forgotPasswordMailgenContent,
+  sendEmail,
+};
